ef311fe316ba97e711fd6e1df0536de9de4787b6
milicaa1405 <milicamilosavljevic14@gmail.com>
2023-01-15 23:05:13 +0100

    Ažurirano pretraživanje

--- reactapp/src/komponente/NavMeni.jsx
+++ reactapp/src/komponente/NavMeni.jsx
@@ -107,18 +107,13 @@ const NavMeni = ({
             <input
               className="form-control me-2"
               type="search"
-              placeholder="Search"
+              placeholder="Pretražite proizvode!"
               aria-label="Search"
               onChange={(e) =>
                 setInput(e.target.value)
               }
             />
-            <button
-              className="btn btn-outline-success"
-              type="submit"
-            >
-              Pretražite
-            </button>
+            
           </form>
         </div>
       </div>
